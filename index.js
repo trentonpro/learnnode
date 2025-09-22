0a492e158a13a5a6bb222991405556af24ac95bf
trentonpro <trent.schotter@gmail.com>
2025-09-22 15:22:17 +0300

    hello world

---
+++ index.js
@@ -0,0 +1,46 @@
+
+let num = 1;
+console.log(num);
+console.log(typeof num);
+
+num = 6.7;
+console.log(num);
+console.log(typeof num);
+
+let text = "Hello, World!";
+console.log(text);
+console.log(typeof text);
+
+text = 'H';
+console.log(text);
+console.log(typeof text);
+
+let bool = true;
+console.log(bool);
+console.log(typeof bool);
+
+bool = false;
+console.log(bool);
+console.log(typeof bool);
+
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+let nil = null;
+console.log(nil);
+console.log(typeof nil);
+
+let array = [1, 2, 'hello', 'world', true, [1, 2, 3]];
+console.log(array);
+console.log(typeof array);
+
+let obj = {
+    name: 'Alice',
+    age: 30,
+    hairColor: 'brown',
+    isMale: false
+};
+
+console.log(obj);
+console.log(typeof obj);
