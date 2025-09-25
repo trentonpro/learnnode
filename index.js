f3bf38632127f7c6ed5daa6f57513a9503971492
trentonpro <trent.schotter@gmail.com>
2025-09-25 13:37:57 +0300

    oyhoihi

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
-
-let num = 1;
-console.log(num);
-console.log(typeof num);
-
-num = 6.7;
-console.log(num);
-console.log(typeof num);
-
-let text = "Hello, World!";
+let text = 'Hello ';
+text = text + 'Trent';
 console.log(text);
-console.log(typeof text);
-
-text = 'H';
+text = 'asdsad';
+text = 'dsadaw';
+text = "don't use double quotes unless needed";
+text = 'Don\'t use double quotes\n unless needed';
 console.log(text);
-console.log(typeof text);
-
-let bool = true;
-console.log(bool);
-console.log(typeof bool);
-
-bool = false;
-console.log(bool);
-console.log(typeof bool);
-
-let undef = undefined;
-console.log(undef);
-console.log(typeof undef);
-
-let nil = null;
-console.log(nil);
-console.log(typeof nil);
-
-let array = [1, 2, 'hello', 'world', true, [1, 2, 3]];
-console.log(array);
-console.log(typeof array);
-
-let obj = {
-    name: 'Alice',
-    age: 30,
-    hairColor: 'brown',
-    isMale: false
-};
-
-console.log(obj);
-console.log(typeof obj);
+let name = 'Trent';
+let age = 67;
+let sexuality = 'straight';
+text = 'Your name is ' + name + ' and you are ' + age + ' years old and your sexuality is ' + sexuality + '??!!!';
+console.log(text);
+text = `Your name is ${name} and you are ${age} years old 
+and your sexuality is ${sexuality}!!!`;
+console.log(text);
