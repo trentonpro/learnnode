f55776941404fd75244d73a876f7ec72fd2acf93
trentonpro <trent.schotter@gmail.com>
2025-09-25 15:37:42 +0300

    boon

--- index.js
+++ index.js
@@ -1,60 +1,35 @@
-function hello() {
-    console.log("Hello, world!");
-}
+let bool = true;
+console.log(bool);
+bool = false;
+console.log(bool); 
+bool = true && false;
+bool = false && true;
+bool = false && false;
+bool = true && true;
+console.log(bool);
+bool = true || false;
+bool = false || true;
+bool = false || false;
+bool = true || true;
+console.log(bool);
+bool = !true;
+console.log(bool);
+bool = !false;
+console.log(bool);
 
-function helloName(name) {
-    console.log(`Hello, ${name}!`);
-}
+bool = (true && false || true) && (!false || (false && true));
+console.log(bool);
 
-helloName("Trent");
-helloName("Bob");
-helloName("bsasd");
-
-function hi(name='nameless', age='Infinite') {
-    console.log(`Hi ${name}, you are ${age} years old!`);
-}
-hi("trent", 32);
-hi("bob", 67);
-hi("bsasd", 41);
-hi();
-
-let sum = function(a, b) {
-    return a + b;
-}
-
-let answer = sum(4, 5);
-console.log(answer);
-console.log(sum(10, 20));
-sum (5, 6);
-
-let sub = (a, b) => {
-    return a - b;
-};
-
-console.log(sub(10, 5));
-
-let mult = (a, b) => a * b;
-
-console.log(mult(10, 5));
-
-let root = a => a * a;
-
-console.log(root(10));
-
-let cool = () => console.log('cool');
-
-cool();
-
-let awesome = {
-    party: function() {
-        console.log('PARTY');
-    },
-    cool: () => console.log('cool'),
-    nice () {
-        console.log('nice');
-    }
-};
-
-awesome.party();
-awesome.cool();
-awesome.nice(); 
+bool = 10 > 5;
+bool = 10 < 5;
+bool = 10 > 10;
+bool = 10 < 10;
+bool = 10 == 10;
+bool = '10' == 10;
+bool = '10' === 10;
+bool = 10 >= 10;
+bool = 10 <= 10;
+bool = '10' != 10;
+bool = '10' !== 10;
+bool = [] == '';
+console.log(bool);
