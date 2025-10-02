7fb117abf71ca93f92bf620ca80e9727cb451934
trentonpro <trent.schotter@gmail.com>
2025-10-02 15:40:37 +0300

    YESSSSSSS

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ export default {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 plugins: [
