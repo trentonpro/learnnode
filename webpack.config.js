5d7ec0657afbee6d4d06dadc0dad313314940570
trentonpro <trent.schotter@gmail.com>
2025-10-06 15:59:00 +0300

    add Template parameters

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/views/index.njk',
+      templateParameters: {
+        name: 'Trent',
+        fruits: ['Apple', 'Cherry', 'Mango', 'Pineapple'],
+      },
     }),
     new HtmlWebpackPlugin({
       filename: 'about.html',
