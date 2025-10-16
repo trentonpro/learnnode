e2cd42d8fdba4e38252d8c66814bacded3c72de6
trentonpro <trent.schotter@gmail.com>
2025-10-16 12:58:07 +0300

    Modal example

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,19 @@
 <script setup>
 import { ref } from 'vue';
-import ItemList from '../components/itemList.vue';
 
-let newItem = ref('');
-let i = 1;
-let items = ref([
-{id: i++, name:'Leib', isDone: false},
-{id: i++, name:'Sai', isDone: true},
-{id: i++, name:'Õlu', isDone: false},
-{id: i++, name:'Viin', isDone: true},
-]);
-let code = ref(404);
-function addItem(){
-    items.value.push({id: i++, name: newItem.value, isDone: false});
-    newItem.value = '';
-}
-
-let doneItems = computed(() => {
-    return items.filter(item => item.isDone);
-});
-
-let doneItems = computed(() => {
-    return items.filter(item => item.isDone);
-});
-
+let modalActive = ref(false);
 </script>
 <template>
-<div class="container">
-    <div class="content">
-        <div class="field has-addons">
-            <div class="control is-expanded">
-                <input v-model="newItem" class="input" type="text" placeholder="Add item">
-            </div>
-            <div class="control">
-                <button class="button is-info" @click="addItem">
-                    Add item
-                </button>
-            </div>
-        </div>
-        <h1>{{ newItem }}</h1>
 
-        <ItemList :items="items" title="All Items"></ItemList>
-        <ItemList :items="doneItems" title="Done Items"></ItemList>
-        <ItemList :items="toDoItems" title="To Do Items"></ItemList>
-        <h1>All Items</h1>
-        <ul>
-            <li v-for="item in items">
-                <input type="checkbox" v-model="item.isDone">
-                {{ item.name }}
-            </li>
-        </ul>
-        <input v-model="code" class="input" type="number" placeholder="Enter Error code">
-        <img :src="'https://http.cat/' + code">
-    </div>
+<button class="button-is-primary" @click="modalActive=true">Open modal</button>
+
+<div class="modal" :class="{'is-active': modalActive}">
+  <div class="modal-background" @click="modalActive=false"></div>
+  <div class="modal-content">
+    <p class="image is-4by3">
+      <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
+    </p>
+  </div>
+  <button class="modal-close is-large" aria-label="close" @click="modalActive=false"></button>
 </div>
 </template>
