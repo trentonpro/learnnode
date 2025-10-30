891e01c11d27921f679fe355293dcf13f2440dda
trentonpro <trent.schotter@gmail.com>
2025-10-30 14:19:51 +0200

    cookie clicker finish

--- src/pages/CookieClicker.vue
+++ src/pages/CookieClicker.vue
@@ -1,11 +1,32 @@
-<script setup></script>
-
+<script setup>
+import { ref } from 'vue';
+ 
+let cookies = ref(0);
+ 
+function buyCursor() {
+    cookies.value -= 15;
+    setInterval(() => {
+        cookies.value = Math.floor((cookies.value + 0.1) * 10);
+    }, 1000);
+}
+ 
+</script>
 <template>
-  <div class="columns">
-  <div class="column has-background-primary">
-    <img src="https://pngimg.com/d/cookie_PNG13669.png"></img>
+    <div class="Columns">
+        <div class="column has-background-primary has-text-centered">
+            <h1>{{ cookies }} cookies!</h1>
+            <div @click="cookies++">
+              <img src="https://clipartcraft.com/images/cookie-clipart-transparent-background-5.png"></img>
+            </div>
+        </div>
+        <div class="column has-background-warning">
+            test
+        </div>
+        <div class="column has-background-info">
+            test
+        </div>
+        <div class="column has-background-info">
+            <button class="button is-primary"> Buy cursor</button>
+        </div>
     </div>
-  <div class="column has-background-warning">test</div>
-  <div class="column has-background-info">test</div>
-</div>
 </template>
